8dc95bc24bc09f213e08b6c49993d605daa9aff9
Nikolay <niko.penev@gmail.com>
2025-10-08 17:17:47 +0300

    update curl

--- .github/workflows/check-repos-updates.sh
+++ .github/workflows/check-repos-updates.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo "Stage1 Find Updates"
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
-LATEST_THEMES_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-themes/releases | jq -r '[.[] | select(.prerelease == false)][0].name' | tr -d v)echo "Last release version is $LATEST_RELEASE"
+LATEST_THEMES_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-themes/releases | jq -r '[.[] | select(.prerelease == false)][0].name' | tr -d v)
+echo "Last release version is $LATEST_RELEASE"
 echo "Last Themes release version is $LATEST_THEMES_RELEASE"
 
 function getCurrentVersion {
@@ -38,29 +39,29 @@ do
     sed -i "s/$CURRENT_GLOBAL_THEMES_VERSION/$LATEST_THEMES_RELEASE/g" $file
 done
 
-echo "Stage2 Commit the change"
-reviewers="Dimitar-Goshev"
-echo $reviewers
-BRANCH_NAME="update-dependencies"
-PRs=$(gh pr list | grep "$BRANCH_NAME" || true)
-echo "PRs are:"
-echo $PRs
-echo "Branch is:"
-echo $BRANCH_NAME
-if [ ! -z $PRs ]; then
-    echo "Unmerged pr $BRANCH_NAME"
-else
-    git fetch origin
-    git pull
-    git checkout -b $BRANCH_NAME
-    git config user.email "kendo-bot@progress.com"
-    git config user.name "kendo-bot"
-    git add . && git commit -m "chore: update dependencies"
-    git pull
-    git push -u origin $BRANCH_NAME
-    gh pr create --base master --head $BRANCH_NAME --reviewer $reviewers \
-    --title "Update dependencies $DATE" --body 'Please review and update dependencies'
+# echo "Stage2 Commit the change"
+# reviewers="Dimitar-Goshev"
+# echo $reviewers
+# BRANCH_NAME="update-dependencies"
+# PRs=$(gh pr list | grep "$BRANCH_NAME" || true)
+# echo "PRs are:"
+# echo $PRs
+# echo "Branch is:"
+# echo $BRANCH_NAME
+# if [ ! -z $PRs ]; then
+#     echo "Unmerged pr $BRANCH_NAME"
+# else
+#     git fetch origin
+#     git pull
+#     git checkout -b $BRANCH_NAME
+#     git config user.email "kendo-bot@progress.com"
+#     git config user.name "kendo-bot"
+#     git add . && git commit -m "chore: update dependencies"
+#     git pull
+#     git push -u origin $BRANCH_NAME
+#     gh pr create --base master --head $BRANCH_NAME --reviewer $reviewers \
+#     --title "Update dependencies $DATE" --body 'Please review and update dependencies'
 
-    git diff
-fi
+#     git diff
+# fi
 
